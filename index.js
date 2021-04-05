55cf8e394f933eea0b54baf0e090bf1c3761f0ee
Jordan <jordan.sposito2430@gmail.com>
2021-04-05 14:30:52 +1000

    Gifcommands

--- index.js
+++ index.js
@@ -1,21 +1,34 @@
 const discord = require('discord.js');
 // Get values from config
-const { prefix, token, commands, insults } = require('./config.json');
+const { prefix, token, giphyToken, Vcommands, Gifcommands, insults } = require('./config.json');
 const client = new discord.Client();
+var GphApiClient = require('giphy-js-sdk-core')
 
 isReady = true;
 
 client.on('message', async message => {
-    if (message.content === '!commands'){
+
+    if (message.content.toLowerCase() === '!commands'){
+        message.reply("!Vcommands, !Gifcommands")
+    }
+
+    if (message.content.toLowerCase() === '!Gifcommands'){
         full_cms = ""
-        commands.forEach(function (cmd, index) {
+        Gifcommands.forEach(function (cmd, index) {
+            full_cms = full_cms + "\n" + cmd[0]
+        });
+        message.reply(full_cms)
+    }
+    if (message.content.toLowerCase() === '!Vcommands'){
+        full_cms = ""
+        Vcommands.forEach(function (cmd, index) {
             full_cms = full_cms + "\n" + cmd[0]
         });
         message.reply(full_cms)
     }
 
-   commands.forEach(function (cmd, index) {
-        if (message.content === cmd[0]) {
+   Vcommands.forEach(function (cmd, index) {
+        if (message.content.toLowerCase() === cmd[0]) {
             if (message.member.voice.channel) {
                 if (isReady) {
                     isReady = false;
@@ -35,6 +48,24 @@ client.on('message', async message => {
           }
         }
    });
+
+    Gifcommands.forEach(function (cmd, index) {
+        if (message.content.toLowerCase() === cmd[0]) {
+            giphy.search('gifs', {"q": cmd[1]})
+            .then((response) => {
+                var totalResponses = response.data.length;
+                var responseIndex = Math.floor((Math.random() * 7) + 1) % totalResponses;
+                var responseFinal = response.data[responseIndex];
+
+                message.channel.send({
+                    files: [responseFinal.images.fixed_height.url]
+                })
+            }).catch(() => {
+                message.channel.send("Whoops something went wrong...");
+            })
+        
+        }
+    });
 });
 
 client.login(token)
