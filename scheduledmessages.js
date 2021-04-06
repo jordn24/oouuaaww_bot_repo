167e413b672b401a9da2bd5388e6df8d174b3ad8
Jordan <jordan.sposito2430@gmail.com>
2021-04-06 15:21:57 +1000

    added scheduled message

---
+++ scheduledmessages.js
@@ -0,0 +1,20 @@
+const discord = require('discord.js');
+const fs = require('fs');
+const { MessageAttachment } = require('discord.js');
+
+const { prefix, giphyToken, Vcommands, Gifcommands, Imgcommands, insults } = require('./config.json');
+const client = new discord.Client();
+
+client.on('ready', () => {
+    const imgFolder = 'images/' + Imgcommands[Math.floor((Math.random() * Imgcommands.length))][1]
+
+    fs.readdir(imgFolder, (err, files) => {
+        filePath = imgFolder + "/" + files[Math.floor((Math.random() * files.length))]
+    
+        const attachment = new MessageAttachment(filePath)
+        client.channels.cache.find(channel => channel.id === '342609340500410368').send("Look at this fucking " + insults[Math.floor((Math.random() * insults.length))], attachment)
+    });
+});
+
+
+client.login(token)
